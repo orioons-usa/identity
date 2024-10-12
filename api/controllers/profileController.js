9b0534e0ebb27ff7305ca5e6f2c50c3effee0317
Mashrafi Shahriar <mashrafishahriar@outlook.com>
2024-10-12 06:29:59 +0600

    added fetchall

--- api/controllers/profileController.js
+++ api/controllers/profileController.js
@@ -30,3 +30,14 @@ exports.getProfile = async (req, res) => {
     res.status(500).send("Server error");
   }
 };
+exports.getProfileExtire = async (req, res) => {
+  try {
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(404).json({ msg: "Profile not found" });
+
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser, subscribeUser, checkPayment, paymentSuccess } = require("../controllers/authController");
 const auth = require("../middlewares/authMiddleware");
+const { getProfileExtire } = require("../controllers/profileController");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/fetch", auth, getProfileExtire);
 router.post("/subscribe", auth, subscribeUser);
 router.post("/payment/success/:email", paymentSuccess);
 
