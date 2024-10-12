9616ebaa081b2868e2ae4b24b9fb049fb259d528
Mashrafi Shahriar <mashrafishahriar@outlook.com>
2024-10-12 19:09:32 +0600

    added CORS

--- api/index.js
+++ api/index.js
@@ -8,7 +8,19 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
+
+var whitelist = ['https://id.intredia.com', 'https://api.id.intredia.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+ 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Connect MongoDB
