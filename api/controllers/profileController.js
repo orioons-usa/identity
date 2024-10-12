7d1317b4cc9489a78045fbc4e7521d7a76921488
Mashrafi Shahriar <mashrafishahriar@outlook.com>
2024-10-12 07:49:33 +0600

    fixed profile update

--- api/controllers/profileController.js
+++ api/controllers/profileController.js
@@ -2,12 +2,12 @@ const User = require("../models/User");
 
 // Create or Update User Profile
 exports.updateProfile = async (req, res) => {
-  const { name, email, company, bio, skills, experience, education, projects, awards, customFields } = req.body;
+  const { name, email, image, company, bio, experience, education, socials, id, emails, phones, customFields } = req.body;
   try {
     const user = await User.findOne({id: req.user.id});
     if (!user) return res.status(404).json({ msg: "User not found" });
 
-    user.profile = {name, email, company, bio, skills, experience, education, projects, awards, customFields };
+    user.profile = {name, email, image, company, bio, experience, education, socials, id, emails, phones, customFields };
     
 
     await user.save();
